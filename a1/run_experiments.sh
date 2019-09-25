417ce3a71c4e15a9ab4530da6964d118a75d8121
Komal Kumar <komal.teru@gmail.com>
2019-09-25 12:15:16 -0400

    Added script to run experiments

---
+++ a1/run_experiments.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python a1.py -d collaboration
+python a1.py -d internet
+python a1.py -d powergrid
+python a1.py -d protein
+python a1.py -d phonecalls
+python a1.py -d citation
+python a1.py -d metabolic
+python a1.py -d email
+python a1.py -d www
