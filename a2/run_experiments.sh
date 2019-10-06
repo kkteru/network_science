6b0cf89a8be5f205e7f947e2012050cb60c3a0be
Komal Kumar <komal.teru@gmail.com>
2019-10-06 00:09:39 -0400

    Added experiments script

--- a2/run_experiments.sh
+++ a2/run_experiments.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-python a2_football.py 
-python a2_karate.py 
-python a2_polblogs.py 
-python a2_polbooks.py 
-python a2_strike.py
+python a2.py -t lfr
+python a2.py -t node -d citeseer
+python a2.py -t node -d cora
+python a2.py -t node -d pubmed
+python a2.py -t real -d football
+python a2.py -t real -d polblogs
+python a2.py -t real -d polbooks
+python a2.py -t real -d strike
+python a2_karate.py
